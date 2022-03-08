6b691b90bade156a60fe1595067e0f7f4bc73cbd
Jasmine Osborne <jasosbo@chbc.school.nz>
2022-03-09 09:41:26 +1300

    component 1- v1 -welcome message

--- ChemicalTests/Program.cs
+++ ChemicalTests/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace ChemicalTests
 {
@@ -6,7 +7,29 @@ namespace ChemicalTests
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("---------------------------------------------------------------\n" +
+                "welcome to chemical testing\n" +
+                "You enter a sample of germs and each chemical.\n" +
+                "the tests will show an efficency rating for each sample you enter\n" +
+                "---------------------------------------------------------------\n\n\n" +
+                "Entering live germ sample:\n" +
+                "To enter manualy enter 1 or to randonmly generate enter 2"
+                );
+
+           
+            
+
+
         }
+
+        
+
+
+
+
+
     }
+
+
+
 }
