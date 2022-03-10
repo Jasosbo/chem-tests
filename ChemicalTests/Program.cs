75d4c2e3f19ebfa281a5da5f6cc1928fe928907e
Jasmine Osborne <jasosbo@chbc.school.nz>
2022-03-11 12:25:16 +1300

    component 2 - v2 -chosen chemical added - remaining germs in sample

--- ChemicalTests/Program.cs
+++ ChemicalTests/Program.cs
@@ -5,15 +5,16 @@ namespace ChemicalTests
 {
     class Program
     {
+
+
+
         static void Main(string[] args)
         {
             Console.WriteLine("---------------------------------------------------------------\n" +
-                "welcome to chemical testing\n" +
+                "Welcome to chemical testing!\n" +
                 "You enter a sample of germs and each chemical.\n" +
-                "the tests will show an efficency rating for each sample you enter\n" +
-                "---------------------------------------------------------------\n\n\n" +
-                "Entering live germ sample:\n" +
-                "To enter manualy enter 1 or to randonmly generate enter 2"
+                "The tests will show an efficency rating for each sample you enter.\n" +
+                "---------------------------------------------------------------\n\n" 
                 );
 
 
@@ -24,6 +25,12 @@ namespace ChemicalTests
 
         static void TestChemical()
         {
+            //first live germ sample
+            Console.WriteLine("Entering live germ sample:\n" +
+                "To enter manualy enter 1 or to randonmly generate enter 2"
+                );
+
+
             float germNum = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
 
 
@@ -31,15 +38,37 @@ namespace ChemicalTests
             if (germNum == 1)
             {
                 Console.WriteLine("Enter amount of live germs in sample\n");
+                float liveGerm = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+
             }
             else
             {
                 Random rnd = new Random();
-                float randflo = rnd.Next();
-                Console.WriteLine($"{randflo}");
+                float liveGerm = rnd.Next();
+                Console.WriteLine($"{liveGerm}");
             }
-        }
 
+
+            Console.WriteLine("\nPlease select the chemical being tested:\n\n" +
+                "1. Chlorine\n" +
+                "2. Formaldehyde\n" +
+                "3. Hydrogen Peroxide\n" +
+                "4. Ethanol\n" +
+                "5. Peracetic Acid\n"
+                );
+
+            int chemchoice = Convert.ToInt32(Console.ReadLine());
+
+            
+            //follow up live germ sample
+            Console.WriteLine("\nPlease enter remaining live germs in sample");
+
+            float remGerms = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture.NumberFormat);
+
+
+
+
+        }
 
 
 
